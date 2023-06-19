65a995aa81413080f69e0dafdcbceb96a3db7522
Erwin <101582520+re48jr7q@users.noreply.github.com>
2023-06-19 08:39:03 +0200

    Add project files.

---
+++ inktmengen/inktmengen.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "inktmengen.h"
+
+class InkBlending
+{
+private:
+	
+};
+int main()
+{
+
+	return 0;
+}
+
+
+
+
---
+++ inktmengen/inktmengen.h
@@ -0,0 +1,41 @@
+#define INKTMENGEN_H
+#ifndef INKTMENGEN_H
+
+struct BasicBlendInks
+{
+	double yellow{};
+	double yellow_012{};
+	double orange_021{};
+	double pink{};
+	double warm_red{};
+	double red_032{};
+	double rubine_red{};
+	double rodamine_red{};
+	double process_blue{};
+	double reflex_blue{};
+	double dark_blue{};
+	double purple{};
+	double medium_purple{};
+	double violet{};
+	double black{};
+	double opaque_white{};
+	double transparent_white{};
+	double zilver_877{};
+	double gold_871{};
+	double gold_872{};
+	double gold_873{};
+	double gold_874{};
+	double gold_875{};
+	double gold_876{};
+	double fluor_801{};
+	double fluor_802{};
+	double fluor_803{};
+	double fluor_804{};
+	double fluor_805{};
+	double fluor_806{};
+	double fluor_811{};
+	double fluor_812{};
+};
+#endif // !INKTMENGEN_H
+
+
